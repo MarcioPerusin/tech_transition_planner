3d03aad208422a7e85412bf577508dce9fc8a880
MarcioPerusin <marcioperozin@gmail.com>
2026-07-06 09:51:26 -0300

    feat:Starts the project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tech Transition Planner
+
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "./assets/vite.svg";
+// import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">Tailwind funcionando!</h1>
+    </div>
+  );
+}
+
+export default App;
