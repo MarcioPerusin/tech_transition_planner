42a2068f933a98cfa0b03deaaa9339bf4df4ad78
MarcioPerusin <marcioperozin@gmail.com>
2026-07-08 12:29:14 -0300

    feat: Integrating Main.jsx with the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
-// src/APP.jsx
-import { Routes, Route } from "react-router-dom";
-import MainLayout from "../src/layouts/MainLayout";
-import "./App.css";
-
-// Componentes de exemplo
-function Dashboard() {
-  return <h1 className="text-2xl font-bold">Painel Principal</h1>;
-}
-
-function Configurar() {
-  return <h1 className="text-2xl font-bold">Configurações do Sistema</h1>;
-}
+// src/App.jsx
+import MainLayout from "./layouts/MainLayout";
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
-    <Routes>
-      {/* Rota pai que carrega o layout estrutural */}
-      <Route path="/" element={<MainLayout />}>
-        {/* Rotas filhas */}
-        <index element={<Dashboard />} /> {/* Rota padrão (/) */}
-        <Route path="configurar" element={<Configurar />} />{" "}
-        {/* Rota (/configurar) */}
-      </Route>
-    </Routes>
+    <MainLayout>
+      <AppRouter />
+    </MainLayout>
   );
 }
 
